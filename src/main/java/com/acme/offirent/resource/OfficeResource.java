aec56c6a5096d7e45f0d76f1e96607037723c6ac
Flavio Saavedra <f.saavedra1810@gmail.com>
2020-10-17 12:27:17 -0500

    little fixes

--- src/main/java/com/acme/offirent/resource/OfficeResource.java
+++ src/main/java/com/acme/offirent/resource/OfficeResource.java
@@ -21,4 +21,94 @@ public class OfficeResource {
     private Boolean status;
 
     private String comment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public OfficeResource setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public OfficeResource setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Long getFloor() {
+        return floor;
+    }
+
+    public OfficeResource setFloor(Long floor) {
+        this.floor = floor;
+        return this;
+    }
+
+    public Long getCapacity() {
+        return capacity;
+    }
+
+    public OfficeResource setCapacity(Long capacity) {
+        this.capacity = capacity;
+        return this;
+    }
+
+    public Boolean getAllowResource() {
+        return allowResource;
+    }
+
+    public OfficeResource setAllowResource(Boolean allowResource) {
+        this.allowResource = allowResource;
+        return this;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
+    public OfficeResource setScore(Float score) {
+        this.score = score;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public OfficeResource setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public OfficeResource setPrice(Float price) {
+        this.price = price;
+        return this;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public OfficeResource setStatus(Boolean status) {
+        this.status = status;
+        return this;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public OfficeResource setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
 }
