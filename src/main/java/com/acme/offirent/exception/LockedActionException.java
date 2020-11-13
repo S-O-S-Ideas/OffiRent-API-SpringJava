08c9619e64079b20673c489713c11875fafe0954
Flavio Saavedra <f.saavedra1810@gmail.com>
2020-11-13 17:22:44 -0500

    Little fix

--- src/main/java/com/acme/offirent/exception/LockedActionException.java
+++ src/main/java/com/acme/offirent/exception/LockedActionException.java
@@ -4,13 +4,9 @@ package com.acme.offirent.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ResourceConditionException extends  RuntimeException{
-    public ResourceConditionException(String message) {
+@ResponseStatus(HttpStatus.LOCKED)
+public class LockedActionException extends  RuntimeException{
+    public LockedActionException(String message) {
         super(message);
-    }
-
-    public ResourceConditionException(String resourceName, String fieldName, String fieldValue) {
-        super(String.format("Resource %s can not be changed with %s with value %s",resourceName,fieldName,fieldValue));
     }
 }
